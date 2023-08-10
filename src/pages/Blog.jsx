71329d5bc2db3616a8cdce6030b6a56b8fe07b3a
Oke Babatunde <tundejosilnet@gmail.com>
2023-08-10 23:18:56 +0100

    blog

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -4,7 +4,7 @@ const Blog = () => {
   return (
     <div className="min-h-[80vh]">
       <div>
-        <h3>our patterns</h3>
+        <h3>our pattern</h3>
         <h1>Under Construction</h1>
       </div>
     </div>
